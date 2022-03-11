72648301bb280b7ffd5f6858d0bf7f3924603e6f
kwamboka3 <pkwamboka@flag42.com>
2022-03-11 14:54:16 +0300

    size

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -6,9 +6,9 @@
 int main(void)
 {
 printf("Size of a char: %d bytes(s)\n", sizeof(char));
-printf("Suze of a int:%d byte(s)\n", size of (int));
-printf("Size of a long int: %d bytes(s)\n", sizeof(long int));
-printf("Size of a long: %d bytes(s)\n", sizeof(long long int));
-printf ("Size of a float: %d byte(s)\n", sizeof(float);
+printf("Size of a int: %d bytes(s)\n", sizeof(char));
+printf("Size of a long int : %d byte(s)\n", sizeof(long int));
+printf("Size of a long:%d byte(s)\n", sizeof(long long int));
+printd("Size of a float:%d byte(s)\n", sizeof(float));
 return (0);
 }
