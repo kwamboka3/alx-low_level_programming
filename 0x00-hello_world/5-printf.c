0b5a40310981af96e5914a8a8324bf5152e3c812
kwamboka3 <pkwamboka@flag42.com>
2022-03-11 14:23:34 +0300

    printf function

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ *main - entry point
+ *my printf program
+ *Return: return 1 after executing the program
+ */
+int main(void){
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+
+return (0);
+}
