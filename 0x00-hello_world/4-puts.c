d3588ac3a087ea4deee881f3386938ddbc5e1448
kwamboka3 <pkwamboka@flag42.com>
2022-03-11 13:10:21 +0300

    4 puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+/**
+*main - entry point
+*
+*Return: 0 after printing the function
+*/
+init main(void)
+{
+puts("\"Programming is like building a multilingua puzzle"};
+return (0);
+}
+
+
+[A[A[A[C >> 
+cd ..
+#include <stdio.h>
+/**
+*main - entry point
+*
+*Return: 0 after printing the function
+*/
+init main(void)
+{
+puts("\"Programming is like building a multilingua puzzle"};
+return (0);
+}
+
