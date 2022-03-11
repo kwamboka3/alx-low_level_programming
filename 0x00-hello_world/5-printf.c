7009adef9ee4cfbc0428c70fbd6cd6e8afa1cb53
kwamboka3 <pkwamboka@flag42.com>
2022-03-11 14:24:42 +0300

    printf function

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -4,8 +4,9 @@
  *my printf program
  *Return: return 1 after executing the program
  */
-int main(void){
-	printf("with proper grammar, but the outcome is a piece of art,\n");
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,\n");
 
 return (0);
 }
