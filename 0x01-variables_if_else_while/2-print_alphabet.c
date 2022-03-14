dfa95dd26714a543c7ce685fd4eb401170e0db5d
kwamboka3 <pkwamboka@flag42.com>
2022-03-14 16:18:23 +0300

    char

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: always 0 (success\corect)
+ *
+ */
+int main(void)
+{
+char alph;
+
+for (alph = 'a': alph < = 'z'; alph++)
+{
+putchar(alph);
+}
+putchar('\n');
+return (0);
+}
