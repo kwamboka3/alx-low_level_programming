87d48f887a98aa198876572eb6a8b9df1ab776b2
kwamboka3 <pkwamboka@flag42.com>
2022-03-15 11:26:34 +0300

    commit

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: always 0 (success/correct)
+ *
+ */
+int main(void)
+{
+char base16;
+
+for (base16 = 48 ; base16 <= 57; base16i++)
+{
+putchar(base16);
+}
+for (base16 = 97 ; base16 <= 102; base16++)
+{
+putchar(base16);
+}
+putchar ('\n');
+return (0);
+}
