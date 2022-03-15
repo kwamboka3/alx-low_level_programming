cf9f79678ddb747dc51aca43fe558a6b6caea662
kwamboka3 <pkwamboka@flag42.com>
2022-03-15 09:42:16 +0300

    exceptional

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: always return 0 (success/correct)
+ *
+ */
+int main(void)
+{
+char alph;
+for (alph = 'a'; alph <='z'; alph++)
+{
+if (alph == 'q')
+continue;
+ 
+else if (alph == 'e')
+continue;
+
+putchar (alph);
+}
+
+putchar('\n');
+return (0);
+}
