f40adaf3156df331bad04a71021b749bd82119ce
kwamboka3 <pkwamboka@flag42.com>
2022-03-15 10:57:47 +0300

    reverse

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always o (success/correct)
+ */
+int main(void)
+{
+char alph;
+for (alph = 'z' ; alph ='a' ; alph--)
+{
+putchar (alph);
+}
+putchar ('/n');
+
+return (0);
+}
