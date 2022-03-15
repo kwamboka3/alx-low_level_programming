941f658722629f39f892732826d2261210b20279
kwamboka3 <pkwamboka@flag42.com>
2022-03-15 09:15:45 +0300

    alphabets

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -14,7 +14,7 @@ for (upper_case = 'A'; upper_case <= 'Z'; upper_case++)
 {
 putchar (upper_case);
 }
-for (lower_case = 'a'; lower_case <= 'z'; upper_case++)
+for (lower_case = 'a'; lower_case <= 'z'; lower_case++)
 {
 putchar (lower_case);
 }
