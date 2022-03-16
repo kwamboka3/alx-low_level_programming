adc171fc950579be4943d95f4ded9ec6016165f0
kwamboka3 <pkwamboka@flag42.com>
2022-03-16 12:14:46 +0300

    my third commit

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * main - main
+ * Return: 0
+ */
+
+int main(void)
+{
+int y;
+y = 0;
+
+positive_or_negative(y);
+
+return (0);
+
+}
---
+++ 0x03-debugging/1-main.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+ * main - causes an infinite loop
+ *
+ * Return: 0
+ */
+int main(void)
+{
+int x;
+
+printf("infinite loop incoming :(\n);
+
+x = 0;
+
+/*
+ * while (x < 10)
+ * {
+ * infinite loop - No increase of variable
+ * putchar(x);
+ * }
+ */
+printf("infinite loop avoided! \\o/\n");
+return (0);
+}
