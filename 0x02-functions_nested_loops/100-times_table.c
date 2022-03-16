e91828b68df97131940c1a36043f6e8152561937
kwamboka3 <pkwamboka@flag42.com>
2022-03-16 12:21:49 +0300

    my first commit

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -7,12 +7,11 @@
  */
 int main(void)
 {
-    print_times_table(3);
-    _putchar('\n');
-    print_times_table(5);
-    _putchar('\n');
-    print_times_table(98);
-    _putchar('\n');
-    print_times_table(12);  
-    return (0);
+int n;
+for (n >= 0;n <=15; n++)
+{
+print_times_table(n);
+}
+_putchar('\n');
+return (0);
 }
