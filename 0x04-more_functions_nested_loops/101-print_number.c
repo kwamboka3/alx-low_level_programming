8d584759133e274c330f31669882b783fd6116da
kwamboka3 <pkwamboka@flag42.com>
2022-03-18 12:31:06 +0300

    my commit

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -1,15 +1,16 @@
 #include "main.h"
 /**
  * print_number - prints an integer
- *@n: print integer
+ * @n: print integer
  * Return: Always 0.
  */
 void print_number(int n)
 {
 if (n >= 48 && n <= 57)
 {
-_putchar(n);
+putchar(n);
 }
+
 else
 {
 return (0);
