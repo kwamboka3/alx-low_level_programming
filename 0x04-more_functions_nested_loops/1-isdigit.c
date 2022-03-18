9d9cf7f1a6f159b204c8212fe7de5a48273a283b
kwamboka3 <pkwamboka@flag42.com>
2022-03-18 11:11:04 +0300

    my first commit

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * int_isdigit - checks for digit
+ * @c: print int
+ * Return: Always 0.
+ */ 
+int _isdigit(int c)
+{
+if (c >= 48 && c<=57)
+{
+return (1);
+}
+else
+{
+return (0);
+}
+}
