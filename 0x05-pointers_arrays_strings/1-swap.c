3d0ed91f20d31bd331e78eb26f73600682faa06a
kwamboka3 <87804803+kwamboka3@users.noreply.github.com>
2022-03-21 15:38:26 +0300

    Update 1-swap.c

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -4,10 +4,10 @@
  */
 #include <stdio.h>
 #include "main.h"
-void swap_int(int &a, int &b)
+void swap_int(int *a, int *b)
 {
 int temp = *a;
 *a = *b;
 *b = temp;
-printf("%d, %d\n", a, b);
+printf("%d, %d\n", *a, *b);
 }
