8cb9682e18d72ecbbf8022e9971d309e8e1736f2
kwamboka3 <87804803+kwamboka3@users.noreply.github.com>
2022-03-21 14:50:55 +0300

    Update 1-swap.c

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -1,30 +1,14 @@
 /**
- * C program to swap two variables using a 
- * user defined swap()
- * swap: it swaps two numbers
+ * swap_int: swipes the two numbers
  * Return: always 0
  */
 #include <stdio.h>
 #include "main.h"
-/**
- * This function swaps values pointed by a and b
- */
-void swap(int *a, int *b)
+void swap_int(int *a, int *b);
 {
     int temp = *a;
     *a = *b;
     *b = temp;
-}
-  
-int main()
-{
-    int a, b;
-    printf("Enter Value of a ");
-    scanf("%d", &a);
-    printf("\nEnter Value of b ");
-    scanf("%d", &b);
-    swap(&a, &b);
-    printf("\nAfter Swapping: a = %d, b = %d", a, b);
-    printf("\n");
-    return 0;
+    putchar(a,b);
+    putchar(\n);
 }
