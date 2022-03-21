c850dc9840fa5666242f8cc9af087f419ebf8ed8
kwamboka3 <87804803+kwamboka3@users.noreply.github.com>
2022-03-21 14:47:31 +0300

    Update 0-reset_to_98.c

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,18 +1,15 @@
 #include "main.h"
-#include <stdio.h>
-
 /**
- * reset_to_98 - pass as parameter and update 
- *
- * Return: Always 0
+ * reset_to_98
+ *@*n: print int
+ * Return: Always 0.
  */
 void reset_to_98(int *n)
 {
-    
-int x = 402;
-*n = &x;    
-reset_to_98(&x);
-putchar(*n);
-
+int n;
+n = 402;
+printf("n=%d\n", n);
+reset_to_98(&n);
+printf("n=%d\n", n);
 return (0);
 }
