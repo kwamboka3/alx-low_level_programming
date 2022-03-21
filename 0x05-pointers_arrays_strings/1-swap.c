c937296ed469fa7cedd72d1125cec92d7f47fd52
kwamboka3 <87804803+kwamboka3@users.noreply.github.com>
2022-03-21 15:53:23 +0300

    Update 1-swap.c

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -1,11 +1,11 @@
+#include <stdio.h>
+#include "main.h"
 /**
  * swap_int: swipes the two numbers a and b
  *@a: print int
  *@b: print int
  * Return: always 0
  */
-#include <stdio.h>
-#include "main.h"
 void swap_int(int *a, int *b)
 {
 int temp = *a;
