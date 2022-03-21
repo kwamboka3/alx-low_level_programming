5f89632f38f5395c3e2a07206c9ba642142fe55a
kwamboka3 <pkwamboka@flag42.com>
2022-03-21 14:04:09 +0300

    my commit

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,9 +1,10 @@
 #include "main.h"
 #include <stdio.h>
 /**
-*rest_to_98: takes a pointer and updates the value
-*Return: Always 0.
-*/
+ * rest_to_98: takes a pointer to an int as parameter and updates value
+ * Return: Always 0
+ */
+
 void rest_to_98(int *n)
 {
 *n = 98;
