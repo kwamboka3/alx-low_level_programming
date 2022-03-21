818072943535c552d35390e3f423eb550521f7b5
kwamboka3 <pkwamboka@flag42.com>
2022-03-21 14:15:18 +0300

    my commit

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -4,15 +4,15 @@
 /**
  * reset_to_98 - pass as parameter and update 
  *
- * Return: Always 0.
+ * Return: Always 0
  */
 void reset_to_98(int *n)
 {
-    int n;
+    
+    *n = 402;
+    printf("n=%d\n", *n);
+    reset_to_98(&*n);
+    printf("n=%d\n", *n);
 
-    n = 402;
-    printf("n=%d\n", n);
-    reset_to_98(&n);
-    printf("n=%d\n", n);
     return (0);
 }
