fc84da27b2fbcc58222adaf0d2337b058a56de13
kwamboka3 <pkwamboka@flag42.com>
2022-03-21 14:36:11 +0300

    my first commit

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,24 @@
+// C program to swap two variables using a 
+// user defined swap()
+#include <stdio.h>
+#include "main.h"
+// This function swaps values pointed by a and b
+void swap(int *a, int *b)
+{
+    int temp = *a;
+    *a = *b;
+    *b = temp;
+}
+  
+int main()
+{
+    int a, b;
+    printf("Enter Value of a ");
+    scanf("%d", &a);
+    printf("\nEnter Value of b ");
+    scanf("%d", &b);
+    swap(&a, &b);
+    printf("\nAfter Swapping: a = %d, b = %d", a, b);
+    printf("\n");
+    return 0;
+}
