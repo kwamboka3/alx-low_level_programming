b73b161c17b2e11a56f5dc2fd706a0f81ea62eff
kwamboka3 <87804803+kwamboka3@users.noreply.github.com>
2022-03-22 15:27:13 +0300

    Update 100-times_table.c

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -4,12 +4,11 @@
  *
  * Return: Always 0.
  */
-void print_times_table()
+void print_times_table(int n)
 {
-int n;
+
 print_times_table(n);
-  
-for (n >= 0;n <= 15; n++)
+if (n >= 0;n <= 15; n++)
 {
 break;
 }
