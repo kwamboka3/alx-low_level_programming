d66de363028d6ab5f6546078bd51ad73f30c4134
kwamboka3 <87804803+kwamboka3@users.noreply.github.com>
2022-03-22 11:34:13 +0300

    Update 100-print_comb3.c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -11,16 +11,16 @@ int number_left;
 int number_right;
 
 
-for (number_left = 48; number_right <= 78; number_left++)
+for (number_left = 48; number_right <= 98; number_left++)
 {
-for (number_right = number_left + 1 ; number_right <= 78; number_right++)
+for (number_right = number_left + 1 ; number_right <= 98; number_right++)
 {
 
 putchar(number_left);
 putchar (number_right);
 
 
-if ((number_left == 56) && (number_right == 78))
+if ((number_left == 56) && (number_right == 98))
 {
 break;
 }
