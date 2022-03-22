9ad798cac29a5148430fe62ce45e9b470fd668b5
kwamboka3 <87804803+kwamboka3@users.noreply.github.com>
2022-03-22 09:37:34 +0300

    Create 3-puts.c

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+*_puts - prints a string
+*@str: A pointer to an int that will be changed
+*
+*Return: void which means our answer is correct
+*/
+
+void _puts(char *str)
+{
+char *k;
+int p;
+
+k = str;
+
+for (p = 0; k[p]; p++)
+{
+_putchar (k[p]);
+}
+_putchar('\n');
+}
