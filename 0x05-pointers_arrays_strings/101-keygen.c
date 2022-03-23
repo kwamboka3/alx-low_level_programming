970b08226e540af8d8f27f49cd74d8041f96485e
kwamboka3 <87804803+kwamboka3@users.noreply.github.com>
2022-03-23 12:20:29 +0300

    Update 101-keygen.c

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -8,17 +8,17 @@
  */
 int main(void)
 {
-	int sum;
-	char c;
+int sum;
+char c;
 
-	srand(time(NULL));
-	while (sum <= 2645)
-	{
-		c = rand() % 128;
-		sum += c;
-		putchar(c);
-	}
-	putchar(2772 - sum);
+srand(time(NULL));
+while (sum <= 2645)
+{
+c = rand() % 128;
+sum += c;
+putchar(c);
+}
+putchar(2772 - sum);
 
-	return (0);
+return (0);
 }
