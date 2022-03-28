4393601c20cb52e37086a20938001802097a397d
kwamboka3 <pkwamboka@flag42.com>
2022-03-28 16:14:40 +0300

    my commit

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,12 +1,10 @@
 #include "main.c"
 /**
  * _memset - fills memory with a constant byte
- *
- * @s: pointer to memory area to fill
+ * * @s: pointer to memory area to fill
  * @n: the number of bytes to fill
  * @b: constant byte
- *
- * Return: a pointer to the memory area @s
+ * * Return: a pointer to the memory area @s
  */
 char *_memset(char *s, char b, unsigned int n)
 {
