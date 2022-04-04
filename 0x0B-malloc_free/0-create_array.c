b3aa866f14cd62767b79996a0651c40970c9d6ad
kwamboka3 <pkwamboka@flag42.com>
2022-04-04 10:53:48 +0300

    my commit

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -1,31 +1,19 @@
 #include "main.h"
-#include <stdio.h>
 #include <stdlib.h>
 /**
- * create_array - creates an array of chars and initializes with a special char
- * @c: the address of the memory to print
- * @size: the size of the memory to print
+ * malloc_checked - allocates memory using malloc
  *
- * Return nothing
+ * @b: the number to allocate memory for
+ *
+ * Return: pointer to the allocated memory
  */
-char *create_array(unsigned int size, char c)
-{
-unsigned int 1;
 
-i = 0;
+void *malloc_checked(unsigned int b)
+{
+	unsigned int *i;
 
-while (i < size)
-{
-if (i % 10)
-{
-	_putchar(i);
-}
-if (!(i % 10) && i)
-{
-	_putchar ("\n");
-}
-printf("0x%02x", buffer[i];
-i++;
-}
-_putchar("\n");
+	i = malloc(b);
+	if (i == NULL)
+		exit(98);
+	return (i);
 }
