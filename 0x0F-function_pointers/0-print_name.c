61cababbd4a4abfa1c23a1ab57f538654e9417ab
kwamboka3 <pkwamboka@flag42.com>
2022-04-14 12:22:09 +0300

    my commit

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -5,7 +5,7 @@
  * @f: the function to send the name to
  * Return: nothing
  */
-void print_name(char *name, void (*f)(char *))
+void print_name(char *name, void (*f)(char*))
 {
 	if (!name || !f)
 		return;
