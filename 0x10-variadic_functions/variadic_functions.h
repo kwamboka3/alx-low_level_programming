eec6fa8c109125c65437b227181772eb4d41cfec
kwamboka3 <87804803+kwamboka3@users.noreply.github.com>
2022-04-17 21:47:48 +0300

    Update and rename main.h to variadic_functions.h

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,16 +1,16 @@
-#ifndef _MAIN_H_
-#define _MAIN_H_
+#ifndef _VARIADIC_FUNCTIONS_H_
+#define _VARIADIC_FUNCTIONS_H_
 #include <stdarg.h>
 #include <stdio.h>
 /*
- *  * struct check - paramenters to check against
- *   * @c: type
- *    * @f: the function to send it to
- *     */
+ * struct check - paramenters to check against
+ * @c: type
+ * @f: the function to send it to
+ */
 typedef struct check
 {
-		char *chk;
-			void (*f)(va_list list);
+	char *chk;
+	void (*f)(va_list list);
 } check_t;
 
 int _putchar(char c);
@@ -18,4 +18,4 @@ int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
 void print_all(const char * const format, ...);
-#endif 
+#endif
