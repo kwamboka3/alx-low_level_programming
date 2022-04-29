1d253e16ac69936953aacb7c7c2a6c87b53ff000
kwamboka3 <87804803+kwamboka3@users.noreply.github.com>
2022-04-29 13:26:01 +0300

    Update 0-binary_to_uint.c

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,5 +1,4 @@
 #include "main.h"
-
 /**
  * binary_to_uint - converts a binary number to unsigned int
  * @b: string containing the binary number
@@ -16,10 +15,9 @@ unsigned int binary_to_uint(const char *b)
 
     for (i = 0; b[i]; i++)
     {
-        if (b[i] < '0' || b[i] > '1')
-            return (0);
-        dec_val = 2 * dec_val + (b[i] - '0');
-    }
-
-    return (dec_val);
+     if (b[i] < '0' || b[i] > '1')
+      return (0);
+     dec_val = 2 * dec_val + (b[i] - '0');
+   }
+   return (dec_val);
 }
