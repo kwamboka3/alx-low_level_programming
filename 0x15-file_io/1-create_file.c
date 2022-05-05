9bf289312ecd32ceb6817fef092b577c4219c8fd
kwamboka3 <87804803+kwamboka3@users.noreply.github.com>
2022-05-05 13:07:41 +0300

    Update 1-create_file.c

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,29 +1,41 @@
 #include "main.h"
+#include <unistd.h>
+#include <fcntl.h>
 
 /**
- * create_file - creates a file
- * @filename: the name of the file to create
- * @text_content: a NULL terminated string to write to the file
+ * create_file - Creates a file with read/write permissions
+ * @filename: name of the file
+ * @text_content: the text to write into the file
  * Return: 1 on success, -1 on failure
  */
+
 int create_file(const char *filename, char *text_content)
 {
-    int file, fwrite, i;
+	int len;
+	int fd, wr;
 
-    if (filename == NULL)
-        return (-1);
-    file = open(filename, O_CREAT | O_TRUNC | O_WRONLY, 0600);
-    if (file == -1)
-        return (-1);
+	if (filename == NULL)
+		return (-1);
 
-    if (text_content != NULL)
-    {
-        for (i = 0; text_content[i]; i++)
-            ;
-        fwrite = write(file, text_content, i);
-        if (fwrite == -1)
-            return (-1);
-    }
-    close(file);
-    return (1);
+	if (text_content == NULL)
+	{
+		fd = open(filename, O_WRONLY | O_CREAT, 0600);
+		if (fd == -1)
+			return (-1);
+		return (1);
+	}
+	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
+	if (fd == -1)
+		return (-1);
+
+	for (len = 0; text_content[len]; len++)
+	{}
+	wr = write(fd, text_content, len);
+	if (wr == -1)
+	{
+		close(fd);
+		return (-1);
+	}
+	close(fd);
+	return (1);
 }
