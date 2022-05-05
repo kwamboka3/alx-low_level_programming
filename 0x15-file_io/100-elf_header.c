5c4bd8372bc97fe97c181f3baf8c1010866a2674
kwamboka3 <87804803+kwamboka3@users.noreply.github.com>
2022-05-05 15:36:36 +0300

    Update 100-elf_header.c

--- 0x15-file_io/100-elf_header.c
+++ 0x15-file_io/100-elf_header.c
@@ -29,18 +29,18 @@ int main(int argc, char *argv[])
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 		exit(98);
 	}
-	validate_elf(header->e_ident);
-	print_magic(header->e_ident);
-	print_class(header->e_ident);
-	print_data(header->e_ident);
-	print_version(header->e_ident);
-	print_osabi(header->e_ident);
-	printf("  ABI Version:                       ");
-	printf("%i\n", header->e_ident[EI_ABIVERSION]);
-	print_type(header->e_type, header->e_ident);
-	print_entry(header->e_entry, header->e_ident);
-	free(header);
-	c = close(fd);
+validate_elf(header->e_ident);
+print_magic(header->e_ident);
+print_class(header->e_ident);
+print_data(header->e_ident);
+print_version(header->e_ident);
+print_osabi(header->e_ident);
+printf("  ABI Version:                       ");
+printf("%i\n", header->e_ident[EI_ABIVERSION]);
+print_type(header->e_type, header->e_ident);
+print_entry(header->e_entry, header->e_ident);
+free(header);
+c = close(fd);
 	if (c)
 		dprintf(STDERR_FILENO, "Error: Can't close fd\n"), exit(98);
 	return (0);
