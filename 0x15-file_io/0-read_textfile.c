d491fe6b167ae407149e933d041d301825cb388f
kwamboka3 <pkwamboka@flag42.com>
2022-05-05 13:02:30 +0300

    update

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,32 +1,47 @@
 #include "main.h"
 
 /**
- * read_textfile - reads a text file and prints it to the POSIX standard output
- * @filename: pointer to text in a file
- * @letters: number of letters
- * Return: the actual number of letters it could read and print
+ * read_textfile - reads a text file and prints to POSIX stdout
+ * @filename: name of the file that's read
+ * @letters: the number of letters read and printed
+ *
+ * Return: the actual  number of letters read and printed, 0 if file cannot be
+ * opened or read, 0 if @filename is NULL, 0 if write fails or doesn't write
+ * the expected amount of bytes
  */
+
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-    ssize_t file, fread, fwrite;
-    char *totalSize;
+	char *buff;
+	int fd, rd, wr;
 
-    totalSize = malloc(sizeof(char) * letters);
-    if (totalSize == NULL)
-        return (0);
-    if (filename == NULL)
-        return (0);
+	if (filename == NULL)
+		return (0);
+	buff = malloc(letters);
+	if (buff == NULL)
+		return (0);
 
-    file = open(filename, O_RDONLY);
-    if (file == -1)
-        return (0);
-    fread = read(file, totalSize, letters);
-    if (fread == -1)
-        return (0);
-    fwrite = write(STDOUT_FILENO, totalSize, fread);
-    if (fwrite == -1)
-        return (0);
-    close(file);
-    free(totalSize);
-    return (fwrite);
+	fd = open(filename, O_RDONLY);
+	if (fd == -1)
+	{
+		free(buff);
+		return (0);
+	}
+	rd = read(fd, buff, letters);
+	if (rd == -1)
+	{
+		free(buff);
+		close(fd);
+		return (0);
+	}
+	wr = write(STDOUT_FILENO, buff, rd);
+	if (wr == -1)
+	{
+		free(buff);
+		close(fd);
+		return (0);
+	}
+	close(fd);
+	free(buff);
+	return (wr);
 }
