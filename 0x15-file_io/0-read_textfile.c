01567a99343b5511d0f372550ef4694e443309be
kwamboka3 <87804803+kwamboka3@users.noreply.github.com>
2022-05-05 15:38:45 +0300

    Update 0-read_textfile.c

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,44 +1,31 @@
-#include "main.h"
-#include <stdio.h>
-#include <stdlib.h>
+#include "alx.h"
 
 /**
-* read_textfile - check the code for Holberton School students.
-* @filename: file to read and write
-* @letters: number of letters to read and write.
-* Return: letters printed
-*/
+ * read_textfile - reads a text file and prints it to standard output
+ * @filename: file descripter
+ * @letters: count
+ *
+ * Return: any time size_t
+ */
+
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-ssize_t nletters;
-int file;
-char *text;
-if (!filename)
-	return (0);
-text = malloc(sizeof(char) * letters + 1);
-if (text == NULL)
-	return (0);
-file = open(filename, O_RDONLY);
-if (file == -1)
-{
-	free(text);
-	return (0);
-}
-nletters = read(file, text, sizeof(char) * letters);
-if (nletters == -1)
-{
-	free(text);
-	close(file);
-	return (0);
-}
-nletters = write(STDOUT_FILENO, text, nletters);
-if (nletters == -1)
-{
-	free(text);
-	close(file);
-	return (0);
-}
-free(text);
-close(file);
-return (nletters);
+	int fd, sz, oz;
+	char *buf = malloc(sizeof(char) * letters);
+
+	fd = open(filename, O_RDONLY);
+	if ((filename == NULL) || (fd == -1) || (buf == NULL))
+	{
+		free(buf);
+		return (0);
+	}
+	sz = read(fd, buf, letters);
+	if (sz == -1)
+		return (0);
+	buf[sz] = '\0';
+	oz = write(STDOUT_FILENO, buf, sz);
+	if (oz == -1)
+		return (0);
+	close(fd);
+	return (oz);
 }
